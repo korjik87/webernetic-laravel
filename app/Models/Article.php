722adc9675d17cb322bd0ed626b7cc6336c57a7c
Sergey <sprakapovich@buffergroup.com>
2024-06-25 20:26:31 +0300

    added barryvdh/laravel-ide-helper

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ *
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property int $author
+ * @property string $published_at
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Article newModelQuery()
+ * @method static Builder|Article newQuery()
+ * @method static Builder|Article query()
+ * @method static Builder|Article whereAuthor($value)
+ * @method static Builder|Article whereBody($value)
+ * @method static Builder|Article whereCreatedAt($value)
+ * @method static Builder|Article whereId($value)
+ * @method static Builder|Article wherePublishedAt($value)
+ * @method static Builder|Article whereTitle($value)
+ * @method static Builder|Article whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Article extends Model
 {
     use HasFactory;
