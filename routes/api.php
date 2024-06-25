0d362941287d33a6e604f2df9ce7e6ffa78931c7
Sergey <sprakapovich@buffergroup.com>
2024-06-25 20:52:59 +0300

    API Routes pats

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/articles', [ArticleController::class, 'index'])->middleware('auth:sanctum');
+Route::get('/articles/{id}', [ArticleController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/articles', [ArticleController::class, 'store'])->middleware('auth:sanctum');
+Route::put('/articles/{id}', [ArticleController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/articles/{id}', [ArticleController::class, 'destroy'])->middleware('auth:sanctum');
+
